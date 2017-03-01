0142342c544043d085f2e092c2b86ed6d4a898e2
VadimA <wadimanosov@gmail.com>
2017-03-02 02:54:30 +0400

    test Travis CI integration with Slack

--- src/main/java/com/diplom/sptor/web/ServiceController.java
+++ src/main/java/com/diplom/sptor/web/ServiceController.java
@@ -459,19 +459,12 @@ public class ServiceController {
 
 		response.setContentType(mimeType);
 
-        /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser
-            while others(zip e.g) will be directly downloaded [may provide save as popup, based on your browser setting.]*/
 		response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() +"\""));
-
-
-        /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
-		//response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
 
 		response.setContentLength((int)file.length());
 
 		InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
 
-		//Copy bytes from source to destination(outputstream in this example), closes both streams.
 		FileCopyUtils.copy(inputStream, response.getOutputStream());
 	}
 
